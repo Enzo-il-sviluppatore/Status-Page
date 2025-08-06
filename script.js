761f5e09ab1bf1aee789d4c2d1d717182b8e23e2
Enzo-il-sviluppatore <vincenzo.balistreri12@gmail.com>
2025-08-06 15:27:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ window.addEventListener("load", () => {
     setTimeout(() => {
       loader.style.display = "none";
       document.querySelector(".container").style.display = "block";
+      document.querySelector("footer").style.display = "block";
       listenForStatusChanges();
     }, 800);
   }, 5000);
